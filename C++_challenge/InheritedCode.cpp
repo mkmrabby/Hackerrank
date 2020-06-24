6a23d40a91466d75aa8e75a708b7d512ccb92525
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2020-06-23 20:39:47 -0400

    23 June 2020 @ 8:39 PM

--- C++_challenge/InheritedCode.cpp
+++ C++_challenge/InheritedCode.cpp
@@ -36,8 +36,54 @@ All other validation is handled by the locked code in your editor.
 */
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
+using namespace std;
+
+/* Define the exception here */
+class BadLengthException{
+private:
+    int size;
+public:
+    BadLengthException(int n){
+        size=n;
+    }
+    int what(){
+        return size;
+    }
+};
+
+
+bool checkUsername(string username) {
+    bool isValid = true;
+    int n = username.length();
+    if(n < 5) {
+        throw BadLengthException(n);
+    }
+    for(int i = 0; i < n-1; i++) {
+        if(username[i] == 'w' && username[i+1] == 'w') {
+            isValid = false;
+        }
+    }
+    return isValid;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int T; cin >> T;
+    while(T--) {
+        string username;
+        cin >> username;
+        try {
+            bool isValid = checkUsername(username);
+            if(isValid) {
+                cout << "Valid" << '\n';
+            } else {
+                cout << "Invalid" << '\n';
+            }
+        } catch (BadLengthException e) {
+            cout << "Too short: " << e.what() << '\n';
+        }
+    }
     return 0;
 }
