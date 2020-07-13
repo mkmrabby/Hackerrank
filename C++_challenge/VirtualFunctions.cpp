8c50658c72dfc79bb14e5b6efd6613c58e6ec179
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2020-07-13 11:44:00 -0400

    13 July 2020 @ 11:43 PM

--- C++_challenge/VirtualFunctions.cpp
+++ C++_challenge/VirtualFunctions.cpp
@@ -58,6 +58,83 @@ White 58 87 2
 #include <algorithm>
 using namespace std;
 
+class Person{
+public:
+    int age;
+    string name;
+
+    virtual void getdata(){
+        cin>>name;
+        cin>>age;
+    }
+
+    virtual void putdata(){
+        cout<<name<<" ";
+        cout<<age<<" ";
+    }
+
+};
+
+class Professor: public Person{
+private:
+    int publications;
+    static int cur_id;
+    int id;
+
+public:
+    Professor(){
+        cur_id++;
+        id=cur_id;
+    }
+
+    void getdata(){
+        Person::getdata();
+        cin>>publications;
+    }
+
+    void putdata(){
+        Person::putdata();
+        cout<<publications<<" "<<id<<endl;
+    }
+};
+
+int Professor::cur_id=0;
+
+class Student:public Person{
+private:
+    vector<int> marks;
+    static int cur_id;
+    int id;
+
+public:
+    Student(){
+        cur_id++;
+        id=cur_id;
+    }
+
+    void getdata(){
+        Person::getdata();
+
+        for (int i=0;i<6;i++){
+            int score;
+            cin>>score;
+            marks.push_back(score);
+        }
+    }
+
+    void putdata(){
+        int sum=0;
+
+        for (int &e : marks)
+            sum+=e;
+
+        Person::putdata();
+        cout<<sum<<" "<<id<<endl;
+    }
+};
+
+int Student::cur_id=0;
+
 int main(){
 
     int n, val;
