c49616814f02a1ec631f675cba7edd95d0c521dd
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-01-31 20:30:56 -0500

    31 Jan 2021 @8:30 PM

---
+++ Algorithms/SolveMeFirst.cpp
@@ -0,0 +1,25 @@
+/*
+Add Two Numbers
+ */
+
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solveMeFirst(int a, int b){
+    return a+b;
+}
+
+int main(){
+    int num1, num2, sum;
+    cin >> num1 >> num2;
+    sum=solveMeFirst(num1, num2);
+    cout<<sum;
+    return 0;
+
+}
+
+
