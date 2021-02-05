d6a92f9414bb46fe2237f6381e254f6183ec69fb
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-02-04 22:59:37 -0500

    4 Feb 2021 @10:59 PM

---
+++ Algorithms/PlusMinus.cpp
@@ -0,0 +1,74 @@
+//
+// Created by MdKhurramMonirRabby on 2/4/2021.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the plusMinus function below.
+void plusMinus(vector<int> arr) {
+    int count_zero=0, count_pos=0, count_neg=0;
+    for(int i=0; i<arr.size(); ++i){
+        arr[i]<0 ? count_neg++ : (arr[i]>0 ? count_pos++ : count_zero++);
+    }
+
+    cout << fixed << setprecision(6) << count_pos/(1.0*arr.size()) << "\n" << count_neg/(1.0*arr.size()) <<"\n" << count_zero/(1.0*arr.size());
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
