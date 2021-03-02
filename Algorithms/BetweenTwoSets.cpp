c6be79ac554180f9e4391c6464936c3cb1dd7cc2
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-03-02 13:53:00 -0500

    2 Mar 2021 @1:52 PM

---
+++ Algorithms/BetweenTwoSets.cpp
@@ -0,0 +1,141 @@
+//
+// Created by MdKhurramMonirRabby on 3/2/2021.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+int getTotalX(vector<int> a, vector<int> b) {
+    int ans=0;
+    int maxx = 0, minn = INT_MAX;
+    int size1 = a.size();
+    int size2 = b.size();
+    for(int i= 0; i < size1; i++)
+        if(maxx<a[i])  maxx = a[i];
+    for(int i = 0; i < size2; i++)
+        if(minn>b[i])  minn = b[i];
+
+    if(maxx>minn)
+        return 0;
+    else {
+        int j = maxx;
+        while (j <= minn)
+        {
+            int flag = 0;
+            for(int i = 0; i < size1; i++)
+                if(j%a[i] != 0)
+                    flag = 1;
+
+            for(int i = 0; i < size2; i++)
+                if(b[i]%j != 0)
+                    flag = 1;
+
+            if(flag == 0)
+                ans++;
+
+            j++;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    string brr_temp_temp;
+    getline(cin, brr_temp_temp);
+
+    vector<string> brr_temp = split(rtrim(brr_temp_temp));
+
+    vector<int> brr(m);
+
+    for (int i = 0; i < m; i++) {
+        int brr_item = stoi(brr_temp[i]);
+
+        brr[i] = brr_item;
+    }
+
+    int total = getTotalX(arr, brr);
+
+    fout << total << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+            s.begin(),
+            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+            find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+            s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
