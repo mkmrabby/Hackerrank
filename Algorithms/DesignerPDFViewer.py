24804ffef6bb96ba35dfb2144cb79ba8bf0a9fb4
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-06-18 00:01:50 -0400

    18 June 2021 @12:01 AM

---
+++ Algorithms/DesignerPDFViewer.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 17 23:58:39 2021
+
+@author: Mrinmoy
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    l1=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    d=dict(zip(l1,h))
+    l=[]
+    for i in word:
+        l.append(d[i])
+    return max(l)*1*len(word)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
