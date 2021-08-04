4c01801f6e55b4c28d99fc79dae1f42a0cb3d15d
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-08-04 14:34:12 -0400

    4 Aug 2021 @2:34 PM

---
+++ Algorithms/Jumping_on_the_Clouds.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    jumps=0
+    for i in range(0,len(c),2):
+        jumps+=1
+        if(c[i]==1):
+            i-=1
+    return jumps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
