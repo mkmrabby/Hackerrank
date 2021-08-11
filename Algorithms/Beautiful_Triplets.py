fde28734e6ae04c8bb446acac9226741048eacf5
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-08-11 12:46:36 -0400

    11 Aug 2021 @12:46 PM

---
+++ Algorithms/Beautiful_Triplets.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def beautifulTriplets(d, arr):
+    # Write your code here
+    n=len(arr)
+    res=0
+    
+    for i in range(n):
+        if arr[i]+d in arr and arr[i]+2*d in arr:
+            res+=1
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
