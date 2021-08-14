2ee7c71cee7a0b52c9ec9f87a4fdd98172049b16
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-08-14 11:40:39 -0400

    14 Aug 2021 @11:40 PM

---
+++ Algorithms/Chocolate_Feast.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 14 11:39:43 2021
+
+@author: MdKhurramMonirRabby
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
+
+def chocolateFeast(n, c, m):
+    # Write your code here
+    choco=n//c
+    wp=0
+    eat=0
+    
+    while choco>0:
+        eat+=choco
+        wp+=choco
+        choco=wp//m
+        wp=wp%m
+    
+    return eat
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        c = int(first_multiple_input[1])
+
+        m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
