13e0a34364d8ab6563b3e8d27bbd367571803697
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-08-21 21:30:28 -0400

    21 Aug 2021 @9:30 PM

---
+++ Algorithms/Manasa_and_Stones.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stones' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER a
+#  3. INTEGER b
+#
+
+def stones(n, a, b):
+    # Write your code here
+    ans=set()
+    for i in range(n):
+        ans.add(a*(n-1-i)+b*i)
+    return sorted(ans)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        a = int(input().strip())
+
+        b = int(input().strip())
+
+        result = stones(n, a, b)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
