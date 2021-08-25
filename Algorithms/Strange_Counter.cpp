36ae5da7046687f74be9555c46a9ca596376ad6e
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-08-25 15:38:48 -0400

    25 Aug 2021 @3:38 PM

---
+++ Algorithms/Strange_Counter.cpp
@@ -0,0 +1,68 @@
+//
+// Created by MdKhurramMonirRabby on 8/25/2021.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'strangeCounter' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER t as parameter.
+ */
+
+long strangeCounter(long t) {
+    long v=4;
+
+    while(v<=t){
+        v=v*2+2;
+    }
+
+    return v-t;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    long t = stol(ltrim(rtrim(t_temp)));
+
+    long result = strangeCounter(t);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+            s.begin(),
+            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+            find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+            s.end()
+    );
+
+    return s;
+}
