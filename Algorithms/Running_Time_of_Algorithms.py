bbe0c88df6b794add4ec22c54972e082876eeaf1
Md Khurram Monir Rabby <mrabby@aggies.ncat.edu>
2021-09-05 23:58:35 -0400

    5 Sep 2021@11:58 PM

---
+++ Algorithms/Running_Time_of_Algorithms.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'runningTime' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def runningTime(arr):
+    # Write your code here
+    count=0
+    n=len(arr)
+    for i in range(0,n):
+        for j in range(i+1,n):
+            if arr[i]>arr[j]:
+                count=count+1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
